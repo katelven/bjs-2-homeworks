7a5c845e0ac22877238a8bb4c3cbeb3a886ed64e
katelven <katesha96@gmail.com>
2022-02-09 17:21:07 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,47 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+  let avg = sum / arr.length;
+  avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+  sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+  let sum = 0;
+  let max = func(arrOfArr[0]);
+  for (let i = 0; i < arrOfArr.length; i++) {
+
+    let sum = func(arrOfArr[i]);
+    if (max < sum) {
+      max = sum;
+    }
+  } return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let arrayParams = getArrayParams(arr);
+  return Math.abs(arrayParams.max - arrayParams.min);
 }
