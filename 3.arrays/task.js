d4e3a0cd1b75d96b4fca4da11553d0bc8f686564
katelven <katesha96@gmail.com>
2022-02-09 20:35:12 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
   // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
   // Ваш код
-
+  resultArr = arr.filter(i => i > 0).filter(i => i % 3 === 0).map(i => i * 10);
   return resultArr; // array
 }
